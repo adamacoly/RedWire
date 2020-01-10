09a13334d97fd0aac7c3e946823c61ad8d9efa37
Lyco <adawerekane@gmail.com>
2020-01-11 00:46:26 +0100

    ma commit

---
+++ RedWire/src/DataFixtures/Users.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Repository\RoleRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class Users extends Fixture
+{
+    private $encoder;
+    private $rep;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder,RoleRepository $rep)
+    {
+        $this->encoder = $encoder;
+        $this->rep=$rep;
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+
+        $user = new User();
+        $role=$this->rep->findOneBy(["libelle"=>"SupAdmin"]);
+        $user->setProfil($role)
+        ->setRoles(["ROLE_".$role->getLibelle()])
+        ->setNomComplet("SupAdmin")
+        ->setUsername("SupAdmin")
+        ->setIsActive(true)
+       ->setPassword($this->encoder->encodePassword($user, "serviteur"));
+
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
---
+++ RedWire/src/Entity/User.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ApiResource(
+ * collectionOperations={
+ *         "get",
+ *         "post"={"security"="is_granted('ROLE_SupAdmin','ROLE_Admin')" , "security_message"="Acces non Autorisé"}
+ *     },
+ *     itemOperations={
+ *         "get",
+ *         "put"={"security"="is_granted('ROLE_SupAdmin','ROLE_Admin') ","security_message"="Acces non Autorisé"},
+ *     })
+ */
+class User implements AdvancedUserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomComplet;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
+     */
+    private $profil;
+
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+
+        return $roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNomComplet(): ?string
+    {
+        return $this->nomComplet;
+    }
+
+    public function setNomComplet(string $nomComplet): self
+    {
+        $this->nomComplet = $nomComplet;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getProfil(): ?Role
+    {
+        return $this->profil;
+    }
+
+    public function setProfil(?Role $profil): self
+    {
+        $this->profil = $profil;
+
+        return $this;
+    }
+    public function isAccountNonExpired(){
+        return true;
+    }
+    public function isAccountNonLocked(){
+        return true;
+    }
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+    public function isEnabled(){
+        return $this->isActive;
+    }
+
+   
+}
